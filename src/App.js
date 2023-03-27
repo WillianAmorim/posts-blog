fab0c399ec228fa1c94344eb82f62604f7133ef7
Willian <willian.amorim1@outlook.com>
2023-03-26 22:02:53 -0300

    importando o useState, e jogando a resposta da api para o state posts

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import axios from "axios";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [posts, setPosts] = useState([]);
+
   //Fazendo uso do useEffect para que a função requisitionGetPosts seja chamada quando a página carregar
   useEffect(() => {
     //utilizando a função requisitionGetPosts pois a função UseEffect não aceita async
@@ -9,6 +11,7 @@ function App() {
       const { data: allPosts } = await axios.get(
         "https://jsonplaceholder.typicode.com/posts"
       );
+      setPosts(allPosts);
     };
 
     requisitionGetPosts();
