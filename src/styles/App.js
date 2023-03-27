c3794d1c52d40834255d479aa90d4c9170397017
Willian <willian.amorim1@outlook.com>
2023-03-27 13:44:01 -0300

    adicionado estilização de posts e img user junto de estilização global do conponente container

---
+++ src/styles/App.js
@@ -0,0 +1,38 @@
+import styled from "styled-components";
+
+export const Container = styled.div `
+  background-color: #f0f2f5;
+  padding: 20px;
+
+  display: flex;
+  flex-direction: column;
+  gap: 50px;
+
+  .posts {
+    border-radius: 15px;
+    background-color: #FFFFFF;
+    display: flex;
+    flex-direction: column;
+    gap: 20px;
+    padding-left: 20px;
+  }
+
+  .img-user {
+    display: flex;
+    img {
+      width: 40px;
+      border-radius: 50px;
+      margin-right: 15px;
+    }
+
+    p {
+      font-family: 'Open Sans', sans-serif;
+      font-size: 20px;
+      font-weight: bold;
+    }
+  }
+
+
+
+  
+`
