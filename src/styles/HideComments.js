cc5717995edf957784666396855d39f56f0868ea
Willian <willian.amorim1@outlook.com>
2023-03-27 17:02:51 -0300

    adicionado estilização do componente

---
+++ src/styles/HideComments.js
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+export const Container = styled.div `
+  display: flex;
+  justify-content: center;
+  gap: 10px;
+  align-items: center;
+  margin-bottom: 10px;
+
+  button {
+    font-size: 20px;
+    border: none;
+    background-color: transparent;
+    color: #7d7372;
+  }
+
+  #button-show:active {
+    opacity: 0.7;
+  }
+`
