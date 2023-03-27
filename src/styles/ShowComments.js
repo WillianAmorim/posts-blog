76a24f9d8f1c28e2d6d13d8b2b9739c6e401b003
Willian <willian.amorim1@outlook.com>
2023-03-27 16:59:15 -0300

    adicionado estilização de button que mostra comentários

--- src/styles/ShowComments.js
+++ src/styles/ShowComments.js
@@ -1,5 +1,20 @@
 import styled from "styled-components";
 
 export const Container = styled.div `
-  
+  display: flex;
+  justify-content: center;
+  gap: 15px;
+  align-items: center;
+  margin-bottom: 5px;
+
+  button {
+    font-size: 20px;
+    border: none;
+    background-color: transparent;
+    color: #7d7372;
+  }
+
+  #button-show:active {
+    opacity: 0.7;
+  }
 `
