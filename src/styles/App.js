b3f94ab06f9634471673ee44cec4ca4b13731685
Willian <willian.amorim1@outlook.com>
2023-03-27 14:49:06 -0300

    estilizando a class title body

--- src/styles/App.js
+++ src/styles/App.js
@@ -32,7 +32,44 @@ export const Container = styled.div `
   }
 
   .title-body {
-    h3 {
+    margin-left: 50px;
+    margin-right: 50px;
+    #p-title {
+      font-size: 20px;
+      font-weight: normal;
+    }
+
+    #p-body {
+      font-size: 15px;
+      font-weight: normal;
+      margin-bottom: 15px;
+    }
+
+    .action-buttons {
+      display: flex;
+      justify-content: space-evenly;
+      text-align: center;
+      align-items: center;
+      border-top: 1px solid #D3D3D3;
+      border-bottom: 1px solid #D3D3D3;
+
+      button {
+        width: auto;
+        border: none;
+        background-color: transparent;
+        display: flex;
+        align-items: center;
+        gap: 10px;
+        padding-top: 15px;
+        padding-bottom: 15px;
+
+        p {
+          font-size: 20px;
+          font-weight: 500;
+          color: #7d7372;
+        }
+
+      }
 
     }
   }
