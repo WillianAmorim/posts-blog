74b64a0c699ef5a4a4a58fcbce80fd40a7e532db
Willian <willian.amorim1@outlook.com>
2023-03-27 17:52:35 -0300

    adicionado div para buttons

--- src/components/Comments.js
+++ src/components/Comments.js
@@ -1,14 +1,31 @@
 import React from 'react'
+import { Container } from '../styles/Commentes'
+import Avatar from '../assets/avatar.png'
 
 const comentários = ({coment}) => {
   return (
-    <div>
-      <ul>
-        <li>{coment.body}</li>
-        <li>{coment.email}</li>
-        <li>{coment.name}</li>
-      </ul>
-    </div>
+    <Container>
+      <div id='div-comment'>
+        <img src={Avatar} alt="" />
+        <ul>
+          <li>{coment.email}</li>
+          <li>{coment.body}</li>
+        </ul>
+      </div>
+
+      <div id='div-buttons'>
+        <button id='button-like'>
+          <p>Curtir</p>
+          <i class="bi bi-hand-thumbs-up"></i>
+        </button>
+
+        <button id='button-comment'>
+          <p>Comentar</p>
+          <i class="bi bi-chat-right"></i>
+        </button>
+      </div>
+      
+    </Container>
   )
 }
 
