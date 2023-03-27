c47ce9226c39dfdf7f40294dfd90be05a5c91dc9
Willian <willian.amorim1@outlook.com>
2023-03-26 22:01:47 -0300

    adicionado useEffect no App com requisição de todos os posts

--- src/App.js
+++ src/App.js
@@ -1,6 +1,19 @@
 import axios from "axios";
+import { useEffect } from "react";
 
 function App() {
+  //Fazendo uso do useEffect para que a função requisitionGetPosts seja chamada quando a página carregar
+  useEffect(() => {
+    //utilizando a função requisitionGetPosts pois a função UseEffect não aceita async
+    const requisitionGetPosts = async () => {
+      const { data: allPosts } = await axios.get(
+        "https://jsonplaceholder.typicode.com/posts"
+      );
+    };
+
+    requisitionGetPosts();
+  }, []);
+
   return (
     <>
     </>
