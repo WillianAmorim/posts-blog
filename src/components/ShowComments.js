679ea0b6e40425579e01a1c97b2e343743dc5c65
Willian <willian.amorim1@outlook.com>
2023-03-27 16:58:54 -0300

    adicionado styled no componente, e adicionado iconte de seta

--- src/components/ShowComments.js
+++ src/components/ShowComments.js
@@ -1,12 +1,14 @@
 import React from 'react'
+import { Container } from '../styles/ShowComments'
 
 const ShowComments = ({getCommets, post}) => {
   return (
-    <div>
-      <button onClick={() => getCommets(post.id)}>
+    <Container>
+      <button id='button-show' onClick={() => getCommets(post.id)}>
         Mostrar Comentários
       </button>
-    </div>
+      <i class="bi bi-chevron-compact-down"></i>
+    </Container>
   )
 }
 
