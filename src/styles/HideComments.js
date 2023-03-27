3a0438ee926c322d94ce3d8afad56f6c361be9fb
Willian <willian.amorim1@outlook.com>
2023-03-27 19:20:47 -0300

    alterado font-size

--- src/styles/HideComments.js
+++ src/styles/HideComments.js
@@ -3,12 +3,11 @@ import styled from "styled-components";
 export const Container = styled.div `
   display: flex;
   justify-content: center;
-  gap: 10px;
   align-items: center;
   margin-bottom: 10px;
 
   button {
-    font-size: 20px;
+    font-size: 10px;
     border: none;
     background-color: transparent;
     color: #7d7372;
